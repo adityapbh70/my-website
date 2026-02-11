705226e80de9c07eccefbb261a557ad5e83809cf
adityapbh70 <adityapbh70@gmail.com>
2026-02-11 17:52:05 +0530

    Add basic Snake game implementation in JavaScript

---
+++ script.js
@@ -0,0 +1,81 @@
+const canvas = document.getElementById("snakeGame");
+const ctx = canvas.getContext("2d");
+const scoreVal = document.getElementById("scoreVal");
+const overlay = document.getElementById("overlay");
+
+let box = 20;
+let score = 0;
+let snake = [{ x: 10 * box, y: 10 * box }];
+let food = {
+    x: Math.floor(Math.random() * 19 + 1) * box,
+    y: Math.floor(Math.random() * 19 + 1) * box
+};
+let d;
+
+document.addEventListener("keydown", direction);
+
+function direction(event) {
+    if(event.keyCode == 37 && d != "RIGHT") d = "LEFT";
+    else if(event.keyCode == 38 && d != "DOWN") d = "UP";
+    else if(event.keyCode == 39 && d != "LEFT") d = "RIGHT";
+    else if(event.keyCode == 40 && d != "UP") d = "DOWN";
+}
+
+function draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    for(let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = (i == 0) ? "#00ff88" : "#008844";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    ctx.fillStyle = "#ff0055";
+    ctx.shadowBlur = 10;
+    ctx.shadowColor = "#ff0055";
+    ctx.fillRect(food.x, food.y, box, box);
+    ctx.shadowBlur = 0;
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if( d == "LEFT") snakeX -= box;
+    if( d == "UP") snakeY -= box;
+    if( d == "RIGHT") snakeX += box;
+    if( d == "DOWN") snakeY += box;
+
+    if(snakeX == food.x && snakeY == food.y) {
+        score++;
+        scoreVal.innerText = score;
+        food = {
+            x: Math.floor(Math.random() * 19 + 1) * box,
+            y: Math.floor(Math.random() * 19 + 1) * box
+        };
+    } else {
+        snake.pop();
+    }
+
+    let newHead = { x: snakeX, y: snakeY };
+
+    if(snakeX < 0 || snakeY < 0 || snakeX >= canvas.width || snakeY >= canvas.height || collision(newHead, snake)) {
+        clearInterval(game);
+        overlay.style.display = "flex";
+    }
+
+    snake.unshift(newHead);
+}
+
+function collision(head, array) {
+    for(let i = 0; i < array.length; i++) {
+        if(head.x == array[i].x && head.y == array[i].y) return true;
+    }
+    return false;
+}
+
+function resetGame() {
+    location.reload(); // Page refresh karke game reset
+}
+
+let game = setInterval(draw, 100);
